5eeacc01db516d02ce58a310a7af111045c5a94f
Chandrabhan Aher <chandrabhanaher@gmail.com>
2025-02-07 12:06:15 +0530

    update drawer menu

--- lib/screens/fliters.dart
+++ lib/screens/fliters.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class FilterScreen extends StatefulWidget {
-  const FilterScreen({super.key})
+  const FilterScreen({super.key});
 
-@override
+  @override
   State<FilterScreen> createState() {
     return _FilterScreenState();
   }
 }
 
-class _FilterScreenState extends State<FilterScreen>{
+class _FilterScreenState extends State<FilterScreen> {
   var _glutenFreeFilterSet = false;
   @override
   Widget build(BuildContext context) {
@@ -20,26 +20,28 @@ class _FilterScreenState extends State<FilterScreen>{
       body: Column(
         children: [
           SwitchListTile(
-            value: _glutenFreeFilterSet, 
-            onChanged: (isChecked){
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
               setState(() {
-                  _glutenFreeFilterSet = isChecked;
+                _glutenFreeFilterSet = isChecked;
               });
             },
             title: Text(
               'Gluten-free',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground
-              ),
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
             ),
             subtitle: Text(
               'Onlu include gluten-free meals',
-              style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground
-              ),
+              style: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
             ),
             activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(left :34, right :22),
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
           ),
         ],
       ),
